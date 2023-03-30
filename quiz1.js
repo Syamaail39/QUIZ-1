80a3f4c5f691a8742c22492614b67e2756ea34ff
Syamaail39 <syamaail39@gmail.com>
2023-03-30 16:03:22 +0800

    quiz1.js

--- quiz1.js
+++ quiz1.js
@@ -1,31 +1,31 @@
-//let message = "benr"
-//console.log(message, typeof message)
+let message = "benr"
+console.log(message, typeof message)
 
-//message = 123
-//console.log(message, typeof message)
+message = 123
+console.log(message, typeof message)
 
 
 
-//let str = "1.258"
-//let num = 1
+let str = "1.258"
+let num = 1
 
-//let total = parseFloat(str) + num
-//console.log(total, typeof total)
+let total = parseFloat(str) + num
+console.log(total, typeof total)
 
-//let data = ['benr',2343,true]
+let data = ['benr',2343,true]
 
-//console.log(data[0])
-//console.log(data[1])
-//console.log(data[2])
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
 
-//let user = {
-    //name: "mael",
-    //age: 23,
-    //faculty: "FKEKK"
-//}
-//console.log(user.name)
-//console.log(user.age)
-//console.log(user.faculty)
+let user = {
+    name: "mael",
+    age: 23,
+    faculty: "FKEKK"
+}
+console.log(user.name)
+console.log(user.age)
+console.log(user.faculty)
 
 
 let messages = [
